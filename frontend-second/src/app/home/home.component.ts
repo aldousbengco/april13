98192cad2957f7e5303191c6509442e1483cc322
Samiul Islam <samiulislamw.w.w@gmail.com>
2021-09-10 10:07:18 +0600

    solve hostResolver error :)

--- frontend-second/src/app/home/home.component.ts
+++ frontend-second/src/app/home/home.component.ts
@@ -2,14 +2,11 @@
 import { User } from '@app/_models';
 import { AccountService } from '@app/_services';
 
-@Component({ templateUrl: 'home.component.html' })
+@Component({ templateUrl: './home.components.html', styleUrls: [] })
 export class HomeComponent {
-    user: User;
+  user: User;
 
-
-
-
-    constructor(private accountService: AccountService) {
-        this.user = this.accountService.userValue;
-    }
+  constructor(private accountService: AccountService) {
+    this.user = this.accountService.userValue;
+  }
 }
